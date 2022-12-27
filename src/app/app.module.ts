ff13de08923d46751da4441fe38cbaed4bf5d8d9
System Administrator <root@Rimjeets-MacBook-Air.local>
2022-12-27 12:16:20 +0530

    new version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { HttpModule } from '@angular/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { ErrorComponent } from './error/error.component';
-import { ProfileComponent } from './profile/profile.component';
-
-
-import { LoginService } from './service/login.service';
-
 
 @NgModule({
   declarations: [
-    AppComponent, LoginComponent, ErrorComponent, ProfileComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule, FormsModule, ReactiveFormsModule, HttpModule,
-    RouterModule.forRoot([{path: 'profile/:id', component: ProfileComponent},
-    {path: 'login', component: LoginComponent},
-    {path: '', component: LoginComponent},
-    {path: '**', component: ErrorComponent}])
+    BrowserModule,
+    AppRoutingModule
   ],
-  providers: [LoginService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
